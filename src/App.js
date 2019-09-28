6ac80fec77bb15504ced0db76e7a5e1ed330c83e
paulyo19 <catkillercleary@gmail.com>
2019-09-28 22:10:44 +0100

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -15,17 +15,16 @@ class App extends Component {
 
     onSearchChange = (event) =>{
         this.setState({ searchfield: event.target.value})
+    }
+    render(){
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        console.log(filteredRobots);
-    }
-    render(){
         return (
             <div className='tc'>
             <h1>RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots={this.state.robots}/>
+            <CardList robots={filteredRobots}/>
             </div>
         );
     }
