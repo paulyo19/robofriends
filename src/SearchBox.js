c886b6cdf0be1a4dad01d479ecfe24b70e2feb3e
paulyo19 <catkillercleary@gmail.com>
2019-09-28 21:10:05 +0100

    added new file that adds a search bar to the app js file

---
+++ src/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import SearchBox from './SearchBox';
+
+const SearchBox = () => {
+    return (
+        <input type='search' placeholder='search robots' />
+    );
+}
+
+export default SearchBox;
