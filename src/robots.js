6426dd02c495c8d3ca786056f6835f35e1c833f2
paulyo19 <catkillercleary@gmail.com>
2019-09-28 16:23:05 +0100

    creating cards with robot images with names and emails

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1, 
+        name: 'Paul Cleary',
+        username: 'Paulyo',
+        email: 'Saul.cleary@gmail.com'
+    },
+    {
+        id: 2, 
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@gmail.com'
+    },
+    {
+        id: 3, 
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@Melissa.tv'
+    },
+    {
+        id: 4, 
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 5, 
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Sarah.OConner@terminator.skynet'
+    },
+    {
+        id: 6, 
+        name: 'James Patrick',
+        username: 'Ghost',
+        email: 'power@starz.net'
+    },
+    {
+        id: 7, 
+        name: 'Tommy Eagan',
+        username: 'Eminem',
+        email: 'Angela@starz.net'
+    },
+    {
+        id: 8, 
+        name: 'Lionel Messi',
+        username: 'Goat',
+        email: 'barca@spain.tv'
+    },
+]
