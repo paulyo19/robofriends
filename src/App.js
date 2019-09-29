2933cd38b465ed0ab4ac3e17764a3ce7163a2c53
paulyo19 <catkillercleary@gmail.com>
2019-09-29 16:10:28 +0100

    using json placholder users for the robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) =>{
@@ -20,6 +25,9 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
         return (
             <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
@@ -27,6 +35,7 @@ class App extends Component {
             <CardList robots={filteredRobots}/>
             </div>
         );
+    }
     }
 }
 
