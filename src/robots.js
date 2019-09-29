a496c0a6691c573b60b75d0a03cfa9bab45df331
paulyo19 <catkillercleary@gmail.com>
2019-09-29 17:29:58 +0100

    finished project

--- src/robots.js
+++ src/robots.js
@@ -1,62 +1,62 @@
-export const robots = [
-    {
-        id: 1, 
-        name: 'Paul Cleary',
-        username: 'Paulyo',
-        email: 'Saul.cleary@gmail.com'
-    },
-    {
-        id: 2, 
-        name: 'Leanne Graham',
-        username: 'Bret',
-        email: 'Sincere@gmail.com'
-    },
-    {
-        id: 3, 
-        name: 'Ervin Howell',
-        username: 'Antonette',
-        email: 'Shanna@Melissa.tv'
-    },
-    {
-        id: 4, 
-        name: 'Clementine Bauch',
-        username: 'Samantha',
-        email: 'Nathan@yesenia.net'
-    },
-    {
-        id: 5, 
-        name: 'Patricia Lebsack',
-        username: 'Karianne',
-        email: 'Sarah.OConner@terminator.skynet'
-    },
-    {
-        id: 6, 
-        name: 'James Patrick',
-        username: 'Ghost',
-        email: 'power@starz.net'
-    },
-    {
-        id: 7, 
-        name: 'Tommy Eagan',
-        username: 'Eminem',
-        email: 'Angela@starz.net'
-    },
-    {
-        id: 8, 
-        name: 'Lionel Messi',
-        username: 'Goat',
-        email: 'barca@spain.tv'
-    },
-    {
-        id: 9, 
-        name: 'Josh Glad',
-        username: 'Happy',
-        email: 'gilmore@myroom.com'
-    },
-    {
-        id: 10, 
-        name: 'Mary Kane',
-        username: 'T-bone',
-        email: 'sunshine@mars.net'
-    },
-]
+// export const robots = [
+//     {
+//         id: 1, 
+//         name: 'Paul Cleary',
+//         username: 'Paulyo',
+//         email: 'Saul.cleary@gmail.com'
+//     },
+//     {
+//         id: 2, 
+//         name: 'Leanne Graham',
+//         username: 'Bret',
+//         email: 'Sincere@gmail.com'
+//     },
+//     {
+//         id: 3, 
+//         name: 'Ervin Howell',
+//         username: 'Antonette',
+//         email: 'Shanna@Melissa.tv'
+//     },
+//     {
+//         id: 4, 
+//         name: 'Clementine Bauch',
+//         username: 'Samantha',
+//         email: 'Nathan@yesenia.net'
+//     },
+//     {
+//         id: 5, 
+//         name: 'Patricia Lebsack',
+//         username: 'Karianne',
+//         email: 'Sarah.OConner@terminator.skynet'
+//     },
+//     {
+//         id: 6, 
+//         name: 'James Patrick',
+//         username: 'Ghost',
+//         email: 'power@starz.net'
+//     },
+//     {
+//         id: 7, 
+//         name: 'Tommy Eagan',
+//         username: 'Eminem',
+//         email: 'Angela@starz.net'
+//     },
+//     {
+//         id: 8, 
+//         name: 'Lionel Messi',
+//         username: 'Goat',
+//         email: 'barca@spain.tv'
+//     },
+//     {
+//         id: 9, 
+//         name: 'Josh Glad',
+//         username: 'Happy',
+//         email: 'gilmore@myroom.com'
+//     },
+//     {
+//         id: 10, 
+//         name: 'Mary Kane',
+//         username: 'T-bone',
+//         email: 'sunshine@mars.net'
+//     },
+// ]
